b7c2edd8baa27be3ea5f445bb606f0bf549f4770
bora-sen <bahattinborasen@gmail.com>
2023-04-10 20:53:03 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# photobybora website
---
+++ src/LandingPage.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function LandingPage() {
+  return <div className="bg-primary-black text-white h-screen w-full">LandingPage</div>
+}
+
+export default LandingPage
